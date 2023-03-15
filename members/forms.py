bd36f263c9a974d439cda5ecdb7dbe75d0091684
marco <mrc.carrano@gmail.com>
2023-03-15 06:54:20 +0100

    files

--- members/forms.py
+++ members/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+
 class RegisterUserForm(UserCreationForm):
 	email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
 	first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
@@ -18,3 +19,4 @@ class RegisterUserForm(UserCreationForm):
 		self.fields['username'].widget.attrs['class'] = 'form-control'
 		self.fields['password1'].widget.attrs['class'] = 'form-control'
 		self.fields['password2'].widget.attrs['class'] = 'form-control'
+
