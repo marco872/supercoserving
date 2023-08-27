823033a3d80d8845b553f95d6c2e25dbd75cff33
marco <mrc.carrano@gmail.com>
2023-08-27 17:12:35 +0200

    date

--- co_servings/models.py
+++ co_servings/models.py
@@ -565,6 +565,7 @@ class Booking1(models.Model):
     starting = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
@@ -580,6 +581,7 @@ class Booking2(models.Model):
     starting = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
@@ -595,6 +597,7 @@ class Booking3(models.Model):
     starting = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
@@ -611,6 +614,7 @@ class Booking4(models.Model):
     starting = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
