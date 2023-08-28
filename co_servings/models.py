fc29ab8b83313f8223febda0cbffa2313c08b434
marco <mrc.carrano@gmail.com>
2023-08-28 21:05:06 +0200

    liquidity

--- co_servings/models.py
+++ co_servings/models.py
@@ -554,16 +554,11 @@ class Payment1(models.Model):
 
 class Liquidity(models.Model):
 
-    BOOKING = (
-        ('Booking1', 'Booking1'),
-        ('Booking2', 'Booking2'),
-        ('Booking3', 'Booking3'),
-        ('Booking4', 'Booking4'),
-    )
+   
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=200, blank=True, null=True)
-    booking = models.CharField(max_length=200, null=True, choices=BOOKING)
+    
     collateral_amount = models.DecimalField(max_digits=5, decimal_places=2)
     
     date_created = models.DateTimeField(auto_now_add=True, null=True)
