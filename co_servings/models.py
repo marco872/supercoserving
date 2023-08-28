cef753bf98aa0259369240315234d4a5f593b846
marco <mrc.carrano@gmail.com>
2023-08-28 20:09:16 +0200

    code

--- co_servings/models.py
+++ co_servings/models.py
@@ -534,7 +534,8 @@ from django.contrib.auth.models import User
 class Payment1(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     Design_Build_Project = models.CharField(max_length=200, blank=True, null=True)
-    collateral_amount = models.DecimalField(max_digits=5, decimal_places=2)
+    collateral_amount = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
