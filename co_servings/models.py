8039cc004affc4facc20875d05a832ec651874de
marco <mrc.carrano@gmail.com>
2023-08-29 09:07:51 +0200

    code

--- co_servings/models.py
+++ co_servings/models.py
@@ -559,8 +559,7 @@ class Payment1(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
-        return self.name
-
+        return f"Payment1 - {self.Design_Build_Project}" 
 
 
 	
